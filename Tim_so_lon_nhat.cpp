4e20f6a08c86801586f789f63544d264ec0fd479
khacv <94861087+khacv@users.noreply.github.com>
2023-08-01 12:10:20 +0700

    Tìm số lớn nhất

---
+++ Tim_so_lon_nhat.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+	long long max = 0;
+	long long n;
+	while(scanf("%lld ",&n) != -1){
+		if(n > max){
+			max = n;
+		}
+	}
+	printf("%lld", max);
+	return 0;
+}
+
